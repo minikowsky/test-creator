ca1f5fd6fed47fe03a0f8785bf6be7e8561b1058
Dominik Marszalek <marszalek.dominik@gmail.com>
2021-05-21 16:57:44 +0200

    Add basic stuff for mvvm

---
+++ Test Creator/ViewModel/BaseClass/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeWPF.ViewModel.BaseClass
+{
+    // this interface is responsible for notifying view about the changes from model
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        //this is delegate
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // this method let the view know that data from viewmodel layer has changed
+        public void onPropertyChanged(params string[] properties)
+        {
+            if (PropertyChanged != null)
+                foreach (var property in properties)
+                    PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
