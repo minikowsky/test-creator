62be7580bb11dff711ce0c571f7d7fc02401448d
Dominik Marszalek <marszalek.dominik@gmail.com>
2021-06-06 21:49:19 +0200

    Add questionControl props and modify creatorVM

--- Test Creator/View/QuestionControl.xaml.cs
+++ Test Creator/View/QuestionControl.xaml.cs
@@ -24,5 +24,57 @@ namespace Test_Creator.View
         {
             InitializeComponent();
         }
+        public static readonly DependencyProperty QuestionProperty =
+            DependencyProperty.Register(nameof(Question), typeof(string), typeof(QuestionControl));
+        public string Question
+        {
+            get => (string)GetValue(QuestionProperty);
+            set => SetValue(QuestionProperty, value);
+        }
+
+        //Answer 1
+        public static readonly DependencyProperty Answer1Property =
+            DependencyProperty.Register(nameof(Answer1), typeof(string), typeof(QuestionControl));
+        public string Answer1
+        {
+            get => (string)GetValue(Answer1Property);
+            set => SetValue(Answer1Property, value);
+        }
+
+        //Answer 2
+        public static readonly DependencyProperty Answer2Property =
+            DependencyProperty.Register(nameof(Answer2), typeof(string), typeof(QuestionControl));
+        public string Answer2
+        {
+            get => (string)GetValue(Answer2Property);
+            set => SetValue(Answer2Property, value);
+        }
+
+        //Answer 3
+        public static readonly DependencyProperty Answer3Property =
+            DependencyProperty.Register(nameof(Answer3), typeof(string), typeof(QuestionControl));
+        public string Answer3
+        {
+            get => (string)GetValue(Answer3Property);
+            set => SetValue(Answer3Property, value);
+        }
+
+        //Answer 4
+        public static readonly DependencyProperty Answer4Property =
+            DependencyProperty.Register(nameof(Answer4), typeof(string), typeof(QuestionControl));
+        public string Answer4
+        {
+            get => (string)GetValue(Answer4Property);
+            set => SetValue(Answer4Property, value);
+        }
+
+        //Radio Buttons
+        public static readonly DependencyProperty CorrectAnswerProperty =
+           DependencyProperty.Register(nameof(CorrectAnswer), typeof(ICommand), typeof(QuestionControl));
+        public ICommand CorrectAnswer
+        {
+            get => (ICommand)GetValue(CorrectAnswerProperty);
+            set => SetValue(CorrectAnswerProperty, value);
+        }
     }
 }
--- Test Creator/ViewModel/CreatorViewModel.cs
+++ Test Creator/ViewModel/CreatorViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Test_Creator.Model;
 
 namespace Test_Creator.ViewModel
 {
@@ -11,6 +12,13 @@ namespace Test_Creator.ViewModel
 
     class CreatorViewModel : BaseViewModel
     {
+        private List<Model.Question> questions;
+        public CreatorViewModel()
+        {
+            questions.Add(new Model.Question());
+        }
+
+        #region Test to edit
         // Test to edit
         private string testFile;
         public string TestFile { 
@@ -24,14 +32,9 @@ namespace Test_Creator.ViewModel
             //TODO:
 
         }
-
-        //Title
-        private string testTitle;
-        public string TestTitle { get => testTitle; 
-            set {
-                testTitle = value;
-                onPropertyChanged(nameof(TestTitle)); 
-            } }
+        #endregion
+        
+        #region Save to file
         //Save
         private ICommand save;
         public ICommand Save
@@ -46,6 +49,8 @@ namespace Test_Creator.ViewModel
         }
         private bool isCompleted()
         {
+            //checks if all questions have answers and correctanswers   
+            
             return false;
         }
         private string testToText()
@@ -53,31 +58,124 @@ namespace Test_Creator.ViewModel
             //TODO:
             return "";
         }
-        //Question control
+        #endregion  
 
+        # region Question control
+        private string question, answer1, answer2, answer3, answer4;
+        public string Question { get => question;
+            set
+            {
+                question = value;
+                onPropertyChanged(nameof(Question));
+                saveToModel();
+                
+            }
+        }
+        public string Answer1 { get => answer1;
+            set
+            {
+                answer1 = value;
+                onPropertyChanged(nameof(Answer1));
+                saveToModel();
+            }
+        }
+        public string Answer2
+        {
+            get => answer2;
+            set
+            {
+                answer2 = value;
+                onPropertyChanged(nameof(Answer2));
+                saveToModel();
+            }
+        }
+        public string Answer3
+        {
+            get => answer3;
+            set
+            {
+                answer3 = value;
+                onPropertyChanged(nameof(Answer3));
+                saveToModel();
+            }
+        }
+        public string Answer4
+        {
+            get => answer4;
+            set
+            {
+                answer4 = value;
+                onPropertyChanged(nameof(Answer4));
+                saveToModel();
+            }
+        }
+        private int correct;
+        private ICommand correctAnswer;
+        public ICommand CorrectAnswer
+        {
+            get
+            {
+                return correctAnswer ?? (correctAnswer = new RelayCommand(
+                    p => { 
+                        correct = Int32.Parse(p.ToString());
+                        saveToModel();
+                    }, 
+                    p => true));
+            }
+        }
+        private void saveToModel()
+        {
+            string[] answers = new string[4] { answer1, answer2, answer3, answer4 };
 
+            questions[current].UpdateData(question, answers, correct);
+        }
+        private void loadQuestion()
+        {
+            question = questions[current].QuestionContent;
+            answer1 = questions[current].Answers[0];
+            answer2 = questions[current].Answers[1];
+            answer3 = questions[current].Answers[2];
+            answer4 = questions[current].Answers[3];
 
+        }
+        #endregion
+
+        #region Title and question number
+        //Title
+        private string testTitle;
+        public string TestTitle
+        {
+            get => testTitle;
+            set
+            {
+                testTitle = value;
+                onPropertyChanged(nameof(TestTitle));
+            }
+        }
         //Question number
         private int current = 0, total = 0;
         public string QuestionNumber { get => $"{current+1}/{total+1}";}
+        #endregion
+
+        #region Navigation and delete
         //Delete button
         private ICommand delete;
         public ICommand Delete
         {
             get
             {
-                return delete ?? (delete = new RelayCommand(p => 
+                return delete ?? (delete = new RelayCommand(p =>
                 {
-                    Console.WriteLine("delete"); 
+                    Console.WriteLine("delete");
 
-                },p =>
+                }, p =>
                 {
                     if (total == 0) return false;
                     return true;
                 }));
             }
         }
-        //Navigation buttons
+
         private ICommand previous;
         public ICommand Previous
         {
@@ -88,6 +186,7 @@ namespace Test_Creator.ViewModel
                     Console.WriteLine("previous");
                     current--;
                     onPropertyChanged(nameof(QuestionNumber));
+                    loadQuestion();
                 }, p => 
                 { 
                     if (current == 0) return false;
@@ -103,12 +202,21 @@ namespace Test_Creator.ViewModel
             {
                 return next ?? (next = new RelayCommand(p =>
                 { 
-                    Console.WriteLine("next");
+                    Console.WriteLine(question);
+                    Console.WriteLine(answer1);
+                    Console.WriteLine(answer2);
+                    Console.WriteLine(answer3);
+                    Console.WriteLine(answer4);
+                    Console.WriteLine(correct);
                     current++;
-                    if (current > total) total++;
+                    if (current > total)
+                        total++;
+                    else
+                        loadQuestion();
                     onPropertyChanged(nameof(QuestionNumber));
                 }, p => true));
             }
         }
+        #endregion
     }
 }
